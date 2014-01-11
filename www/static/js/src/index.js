7973d3f92bb7ca5a58554e6416d23e4947b0adeb
Elijah Kaytor <elijah+git@kaytor.ca>
2014-01-11 02:24:45 -0800

    JavaScript: Email Suggestions

--- www/static/js/src/index.js
+++ www/static/js/src/index.js
@@ -9,6 +9,83 @@ var Firebase = Firebase;
 var sha256 = sha256;
 
 
+/// Email helper
+function helper() { return $.fn.text.apply($('#domain-helper'), arguments); }
+
+// Aliases
+helper.set = helper.get = helper;
+
+// List of commmon domains
+helper.domains = $('#domains li').map(function(index) {
+    return $('#domains li').eq(index).text();
+});
+// Default domain to choose
+helper.default = helper.domains[0];
+
+helper.complete = function(partialEmail) {
+    
+    // If there is no information, return default  domain
+    if (partialEmail.indexOf('@') === -1) return helper.default;
+    
+    // Get the partial host
+    var partialHost = partialEmail.slice(partialEmail.indexOf('@'));
+    
+    // Generate a list of possible completions
+    var possibleDomains = helper.domains.filter(function(index, domain) {
+        return domain.startsWith(partialHost);
+    });
+    
+    // Return the host completion
+    return (possibleDomains[0] || '').slice(partialHost.length);
+    
+};
+
+$('#email').on('keydown keypress', function(event) {
+    
+    // Get relevant values
+    var value = $('#email').text();
+    var keyCode = event.keyCode || event.which;
+    var character = event.charCode?
+        String.fromCharCode(event.charCode)
+    :   '';
+    
+    // Offer a suggestion
+    helper.set(helper.complete(value + character));
+    
+    
+    // On [SHIFT] [2]
+    if (keyCode == 64 && value.indexOf('@') !== -1) {
+        event.preventDefault();
+        return false;
+    }
+    
+    // On [ENTER]
+    if (keyCode == 13) {
+        event.preventDefault();
+        
+        if (validEmail.exec(value)) $('#submit').click();
+        else if (value.length > 0) {
+            
+            $('#email').append($('#domain-helper').text()).focusEnd();
+            $('#domain-helper').html('');
+            
+        }
+        
+        return false;
+    }
+    
+    // On [TAB]
+    if (keyCode == 9 && value.length > 0) {
+        event.preventDefault();
+        
+        $('#email').append($('#domain-helper').text()).focusEnd();
+        $('#domain-helper').html('');
+        
+        return false;
+    }
+    
+});
+
 /// Handle Form submissions
 // Regexp for email
 var validEmail = /^[\w\.%+-]+@[\w\.-]+\.[A-z]{2,6}$/;
@@ -135,3 +212,29 @@ if (location.hash == '#notify-me') {
     pages.switchTo(3);
     pages.stop();
 }
+
+
+
+/// jQuery focusEnd function
+/// (http://stackoverflow.com/a/14022827)
+$.fn.focusEnd = function() {
+    $(this).focus();
+    var tmp = $('<span />').appendTo($(this)),
+        node = tmp.get(0),
+        range = null,
+        sel = null;
+    
+    if (document.selection) {
+        range = document.body.createTextRange();
+        range.moveToElementText(node);
+        range.select();
+    } else if (window.getSelection) {
+        range = document.createRange();
+        range.selectNode(node);
+        sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+    }
+    tmp.remove();
+    return this;
+};
