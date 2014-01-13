23ca0b2deea615a57d4a17937fb22bdcfce705e8
Elijah Kaytor <elijah+git@kaytor.ca>
2014-01-12 22:17:39 -0800

    Analytics: Refactoring to improve compressibility

--- www/static/js/src/analytics.js
+++ www/static/js/src/analytics.js
@@ -8,14 +8,23 @@
 // Load Google Analytics Asyncronously
 var ga = ga || function() { (ga.q = ga.q || []).push(arguments); };
 window.GoogleAnalyticsObject = 'ga'; ga.l = new Date().getTime();
-$.getScript('//www.google-analytics.com/analytics.js');
 
-// Retrieve GA ID and Site
-var gaID   = $('meta[name=ga-id]').attr('content');
-var gaSite = $('meta[name=ga-site]').attr('content');
-
-// Add actions to the GA Queue
-if (gaID && gaSite) { 
-    ga('create', gaID, gaSite); 
-    ga('send', 'pageview');
-}
+// Create function scope to allow compresssion of variables
+(function() {
+    
+    // Load the analytics script
+    var script = document.createElement('script');
+        script.src = '//www.google-analytics.com/analytics.js';
+    document.head.appendChild(script);
+    
+    // Retrieve GA ID and Site
+    var gaID   = $('meta[name=ga-id]').attr('content');
+    var gaSite = $('meta[name=ga-site]').attr('content');
+    
+    // Add actions to the GA Queue
+    if (gaID && gaSite) {
+        ga('create', gaID, gaSite);
+        ga('send', 'pageview');
+    }
+    
+})();
