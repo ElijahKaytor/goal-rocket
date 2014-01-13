0c38ed1b3dde8877299607d9bd27804ca8579eeb
Elijah Kaytor <elijah+git@kaytor.ca>
2014-01-12 22:36:28 -0800

    Grunt: Updating for new JavaScript resources

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,7 @@
+/*! File: Gruntfile.js
+ *  Author: Elijah Kaytor
+ */
+
 module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     
@@ -6,36 +10,50 @@ module.exports = function(grunt) {
             options: {
                 mangle: true,
                 compress: true,
+                sourceMapPrefix: 3,
                 report: 'min',
             },
             
             'onload.min.js': {
                 options: {
-                    sourceMapPrefix: 3,
                     sourceMap: 'www/static/js/onload.min.map',
                     sourceMappingURL: 'onload.min.map',
+                    banner: '/* File: onload.min.js | Includes: jQuery Core */'
                 },
                 
                 files: {
                     'www/static/js/onload.min.js': [
-                        'www/static/js/src/jquery-1.10.2.js',
+                        'www/static/js/src/jquery-core-2.1.0pre.js',
                         'www/static/js/src/index.js',
                     ],
                 }
             },
             
+            'inline-loader.min.js': {
+                options: {
+                    sourceMap: 'www/static/js/inline-loader.min.map',
+                    sourceMappingURL: '/static/js/inline-loader.min.map',
+                    banner: '/* File: inline-loader.min.js */'
+                },
+                
+                files: {
+                    'www/static/js/inline-loader.min.js': [
+                        'www/static/js/src/inline-loader.js',
+                    ],
+                },
+            },
+            
             'deffered.min.js': {
                 options: {
-                    sourceMapPrefix: 3,
                     sourceMap: 'www/static/js/deffered.min.map',
                     sourceMappingURL: 'deffered.min.map',
+                    banner: '/* File: deffered.min.js */'
                 },
                 
                 files: {
                     'www/static/js/deffered.min.js': [
-                        'www/static/js/src/firebase-v0.min.js',
+                        'www/static/js/src/email-helper.js',
                         'www/static/js/src/analytics.js',
-                        'www/static/js/src/sha-256.js',
                     ],
                 },
             },
