31747cf0e3bb24e23e872c60b730939f656c7e60
Elijah Kaytor <elijah+git@kaytor.ca>
2017-04-25 21:11:46 -0700

    Fix compress option

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,7 +43,7 @@ module.exports = function(grunt) {
         uglify: {
             options: {
                 mangle: true,
-                compress: true,
+                compress: {},
                 sourceMapPrefix: 3,
                 report: 'min',
             },
